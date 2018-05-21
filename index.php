e80181f862a3dbb60d055c9b1ff220376ddd86f4
Tatyana Komarova <komarovats93@gmail.com>
2018-05-21 12:56:19 +0300

    -

--- index.php
+++ index.php
@@ -439,7 +439,7 @@
             resolution: 'standard_resolution',
             template: '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3"><div class="photo-box"><div class="image-wrap"><a href="{{link}}"><img src="{{image}}"></a><div class="likes">{{likes}} Likes</div></div><div class="description">{{caption}}<div class="date">{{model.date}}</div></div></div></div>'
         });
-        feed.run();
+        //feed.run();
     </script>
 </body>
 
